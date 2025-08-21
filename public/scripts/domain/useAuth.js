1344f68eeb1ae81e36cef1179c46ae4bc2a14a10
Cristian <cristianariasu13@gmail.com>
2025-08-20 21:29:29 -0500

    de login.html a index.html

--- public/scripts/domain/useAuth.js
+++ public/scripts/domain/useAuth.js
@@ -1,15 +1,15 @@
-import { supabase } from '../data/supabaseClient.js';
-
-export async function loginWithCredentials(username, password) {
-  const { data, error } = await supabase
+async function loginWithCredentials(username, password) {
+  const { data, error } = await window.supabaseClient
     .from('usuarios')
     .select('*')
     .eq('nombre_usuario', username)
     .eq('contraseña', password)
     .single();
 
-   if (error || !data) {
+  if (error || !data) {
     return { error };
   }
   return { user: data };
 }
+
+window.loginWithCredentials = loginWithCredentials;
--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,20 +1,20 @@
-// Conexión directa con Supabase usando la CDN
-import { loginWithCredentials } from "../scripts/domain/useAuth.js";
 const loginForm = document.getElementById('loginForm'); 
+
 loginForm.addEventListener('submit', async (event) => { 
     event.preventDefault(); 
+    
     const username = document.getElementById('usernameInput').value.trim(); 
     const password = document.getElementById('passwordInput').value.trim(); 
     
-    // Consulta directa a la tabla usuarios 
-    const {user , error} = await loginWithCredentials(username, password); 
+    const { user, error } = await window.loginWithCredentials(username, password); 
+    
     if (error || !user) { 
         alert('Nombre de usuario o contraseña incorrectos'); 
         return; 
-    } 
-    // Guardar sesión 
+    }
+    
     localStorage.setItem('currentUser', user.nombre_usuario); 
     
-    // Redirigir al formulario de visitantes 
-    window.location.href = '/index.html'; 
+    // Redirigir al formulario de visitantes
+    window.location.href = 'pages/visitantes.html';
 });
